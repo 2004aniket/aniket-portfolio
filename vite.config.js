4fe285e345d04b215401508a44b345379b8af794
Aniket_Pise <118109413+2004aniket@users.noreply.github.com>
2025-03-15 10:30:34 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from 'tailwindcss'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"aniket-portfolio",
   assetsInclude:['**/*.glb'],
   css: {
     postcss: {
